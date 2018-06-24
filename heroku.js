7af1aa974637b384297c2eaab20a02724a177e34
antim0 <antim0@users.noreply.github.com>
2018-06-24 19:21:10 +0200

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -2,14 +2,5 @@ const SalienScript = require('./src/index.js');
 
 const SALIEN_CONFIG = process.env.SALIEN_CONFIG;
 
-for (let config_str of SALIEN_CONFIG.split(';')) {
-    [token, clan, name] = config_str.split(':');
-    const config = {
-        token: token,
-        clan: clan,
-        name: name,
-    };
-
-    const salien = new SalienScript(config);
-    salien.init();
-}
+const salien = new SalienScript(config);
+salien.init();

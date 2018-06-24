d90a48221196d4a4e2f3fd6d72ecfae74e2b350b
antim0 <antim0@users.noreply.github.com>
2018-06-24 19:22:52 +0200

    Update heroku.js

--- heroku.js
+++ heroku.js
@@ -2,5 +2,5 @@ const SalienScript = require('./src/index.js');
 
 const SALIEN_CONFIG = process.env.SALIEN_CONFIG;
 
-const salien = new SalienScript(config);
+const salien = new SalienScript();
 salien.init();
